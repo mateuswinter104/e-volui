3d715399bd7c29bbe42033d4c4b6418a777c7a53
wintermateus <mateus.winter@codescript.com.br>
2025-05-09 21:53:25 -0300

    adding dependencies to backend

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+// ⚠️ Carregue as variáveis de ambiente antes de qualquer uso
+dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URI!)
   .then(() => console.log("Conectado ao MongoDB"))
   .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
